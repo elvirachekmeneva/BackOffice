4f92e30b88fac227a6aa0b1ebf0997fdf3999f98
Эльвира Чекменева <elvirachekmeneva@air11.local>
2013-12-20 12:48:50 +0400

    начало работать сохранение логина и пароля в файл и выгрузка их при открытии

--- BackOffice/SignInViewController.h
+++ BackOffice/SignInViewController.h
@@ -10,6 +10,7 @@
 
 @interface SignInViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *signInButton;
 @property (weak, nonatomic) IBOutlet UISwitch *saveOrNot;
 @property (weak, nonatomic) IBOutlet UITextField *userLogin;
 @property (weak, nonatomic) IBOutlet UITextField *userPassword;
--- BackOffice/SignInViewController.m
+++ BackOffice/SignInViewController.m
@@ -10,15 +10,13 @@
 #import <CommonCrypto/CommonDigest.h>
 
 @implementation NSString(MD5)
-- (NSString*)MD5
-{
+- (NSString*)MD5 {
     const char *ptr = [self UTF8String];
     unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
     CC_MD5(ptr, strlen(ptr), md5Buffer);
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
         [output appendFormat:@"%02x",md5Buffer[i]];
-    
     return output;
 }
 
@@ -29,7 +27,7 @@
 @end
 
 @implementation SignInViewController
-@synthesize userLogin, userPassword,connection;
+@synthesize userLogin, userPassword,connection,signInButton;
 @synthesize login,password;
 @synthesize saveOrNot;
 
@@ -42,12 +40,13 @@
 
 - (void)viewDidAppear:(BOOL)animated{
     [[self navigationController] setNavigationBarHidden:YES animated:YES];
-    //загрузка состояния UISwitch
     [super viewWillAppear:animated];
     
 }
 
 - (void)viewWillAppear:(BOOL)animated{
+    
+    //загрузка состояния UISwitch
     BOOL test= [[NSUserDefaults standardUserDefaults] boolForKey:@"switch"];
     NSLog(@"%@",test?@"YES":@"NO");
     [self.saveOrNot setOn:test animated:YES];
@@ -55,13 +54,7 @@
     
     //загрузка логина и пароля из .plist при включенном UISwith
     if (self.saveOrNot.on == YES){
-        NSDictionary *userLoginAndPass = [NSDictionary dictionaryWithContentsOfFile: [[NSBundle mainBundle]
-                                                                    pathForResource:@"login_password" ofType:@"plist"]];
-        NSString *log = [userLoginAndPass objectForKey:@"login"];
-        NSString *pas = [userLoginAndPass objectForKey:@"password"];
-        NSLog(@"login: %@, password: %@", log,pas);
-        [userLogin setText:log];
-        [userPassword setText:pas];
+        [self loadData];
         
     }
 
@@ -72,9 +65,36 @@
     [super didReceiveMemoryWarning];
 }
 
+//получение адреса файла
+- (NSString*)getFilePath {
+    NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    return [[pathArray objectAtIndex:0]stringByAppendingPathComponent:@"login_password.plist"];
+}
+
+//сохранение логина и пароля в .plist
+- (void) saveLogin:(NSString*)login andPassword:(NSString*)password {
+    NSArray *value = [[NSArray alloc] initWithObjects:login,password, nil];
+    [value writeToFile:[self getFilePath] atomically:YES];
+}
+
+//загрузка логина и пароля из .plist прямо в textField'ы
+- (void) loadData {
+    NSString* myPath = [self getFilePath];
+    bool fileExist = [[NSFileManager defaultManager] fileExistsAtPath:myPath];
+    if (fileExist) {
+        NSArray* values = [[NSArray alloc]initWithContentsOfFile:myPath];
+        [userLogin setText:[values objectAtIndex:0]];
+        [userPassword setText:[values objectAtIndex:1]];
+    }
+}
+
 - (IBAction)signInButtonPressed:(id)sender {
     login = [[NSString alloc]initWithString:[userLogin text]];
     password = [[NSString alloc]initWithString:[userPassword text]];
+    
+    if (self.saveOrNot.on == YES){
+        [self saveLogin:login andPassword:password];
+    }
     
     NSString *urlString = [NSString stringWithFormat:@"http://m.bossnote.ru/empl/getUserData.php?login=%@&passwrdHash=%@",login,[password MD5]];
     NSLog(@"url %@", urlString);
@@ -97,7 +117,7 @@
                                                                     options:kNilOptions error:&requestError];
         NSLog(@"json %@", json);
     }
-    
+    /*
     if (self.saveOrNot.on == YES){
         NSMutableDictionary *userLoginAndPass = [[NSMutableDictionary alloc]init];
         [userLoginAndPass setObject:login forKey:@"login"];
@@ -105,7 +125,7 @@
         [userLoginAndPass writeToFile:@"login_password.plist" atomically:YES];
         
     }
-    
+    */
     
     
 }
